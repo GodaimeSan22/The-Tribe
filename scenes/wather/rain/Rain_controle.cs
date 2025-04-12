148647692eebc45452c77edc6d25e572d1cd8f23
Danylo-Hrom <dhrom58@gmail.com>
2025-04-12 20:11:13 +0300

    added interval rain timer

--- scenes/wather/rain/Rain_controle.cs
+++ scenes/wather/rain/Rain_controle.cs
@@ -2,7 +2,9 @@ using Godot;
 
 public partial class Rain_controle : StaticBody2D
 {
-	public bool currentWeather = false;
+	public bool currentWeather = false; 
+
+	private Timer timer;
 
 	public override void _Ready()
 	{
@@ -18,6 +20,35 @@ public partial class Rain_controle : StaticBody2D
 		{
 			rainParticles.Visible = true;
 			rainShading.Visible = true;
-		} 
+		}
+
+		timer = new Timer();
+		AddChild(timer);
+		timer.Start(); 
+
+		timer.Timeout += () =>
+		{
+			currentWeather = !currentWeather;
+
+			rainParticles.Visible = currentWeather;
+			rainShading.Visible = currentWeather;
+
+			// output status to the console
+			GD.Print("Weather Changed: " + (currentWeather ? "Rainy" : "Clear"));
+
+			if (currentWeather)
+			{
+				// if wather is rainy
+				timer.WaitTime = 20.0f;
+			}
+			else
+			{
+				// if wather is clear
+				timer.WaitTime = 120.0f;
+			}
+			timer.Start();
+		};
+
+		timer.WaitTime = currentWeather ? 20.0f : 120.0f;
 	}
 }
